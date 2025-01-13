41690057bbe1d2f5e5d3de14f371b3e32dc52ba9
Craig Stacey <craig.stacey@southwales.ac.uk>
2025-01-13 23:02:05 +0000

    removed unused code

--- app/src/main/java/fit/magic/cv/repcounter/ExerciseRepCounterImpl.kt
+++ app/src/main/java/fit/magic/cv/repcounter/ExerciseRepCounterImpl.kt
@@ -18,16 +18,12 @@ data class Vector(val x: Double, val y: Double, val z: Double)
 data class Keypoint(val x: Double, val y: Double, val z: Double)
 class ExerciseRepCounterImpl : ExerciseRepCounter() {
 
-    private var frameCountForTimestamp = 0
-    private var currentTimestamp : Long = 0
-    private var previousTimestamp : Long = 0
     private var previousFrame  = emptyList<Keypoint>()
     private var halfRep = false
     private var isFirst = true
     private var idealAngle = 95.0
     private var maxAngle = 150.0
-    private var maxDistance = 0.0
-    private var frameCount = 25
+    private var frameCount = 20
 
     private var currentOrientation: Double = 0.0
     private var movingAverage = MovingAverage(frameCount)
@@ -46,7 +42,6 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
         poseKalmanFilter.initializeFilters(33)
 
         if (resultBundle.results[0].landmarks().isNotEmpty()) {
-            currentTimestamp = System.currentTimeMillis()
             var poseKeypoints = preprocessKeypoints(resultBundle.results[0].landmarks()[0])
 
             orientationMovingAverage.add(getOrientation(poseKeypoints))
@@ -60,28 +55,12 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
                 movingAverage.clear()
                 orientationMovingAverage.clear()
             }
-            //else if((previousFrame.isNotEmpty()) && (previousTimestamp != 0L))
-              //  poseKeypoints =  checkForGlitch(previousFrame = previousFrame, currentFrame = poseKeypoints)
 
             if (isFirst) {
                 currentOrientation = orientation
                 previousFrame = poseKeypoints
-                maxDistance = (poseKeypoints[28].y - poseKeypoints[26].y).toDouble()
                 isFirst = false
             }
-
-            var rotatedKeypoints: MutableList<Keypoint> = mutableListOf()
-
-            //val preRotation = orientation
-            //poseKeypoints = rotateKeypoints(poseKeypoints).toMutableList()
-            //val postRotation = getOrientation(poseKeypoints)
-
-            //sendFeedbackMessage(
-            //    "preRotation:" + String.format("%.2f", preRotation) + "\r\n" +
-            //    "postRotation:" + String.format("%.2f", postRotation) + "\r\n"
-            //)
-            //poseKeypoints = adjustZAxisPredictions(rotatedKeypoints, poseKeypoints)
-            //poseKeypoints = rotatedKeypoints
 
             poseKeypoints = poseKalmanFilter.update(poseKeypoints)
 
@@ -94,16 +73,8 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
                     idealAngle = 90.0
                     updateProgress(poseKeypoints)
                 }
-                //if (!halfRep && ((getAngle(poseKeypoints[23], poseKeypoints[25], poseKeypoints[27]) < idealAngle) || (getAngle(poseKeypoints[24], poseKeypoints[26], poseKeypoints[28])< idealAngle))) {
-                //    halfRep = true
-                //}
-                //if (halfRep && getAngle(poseKeypoints[23], poseKeypoints[25], poseKeypoints[27]) > maxAngle && getAngle(poseKeypoints[24], poseKeypoints[26], poseKeypoints[28]) > maxAngle) {
-                //    incrementRepCount()
-                //    halfRep = false
-                //}
             }
             previousFrame = poseKeypoints
-            previousTimestamp  = currentTimestamp
         }
         else
             if(previousFrame.isNotEmpty()) {
@@ -164,21 +135,7 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
             incrementRepCount()
             halfRep = false
         }
-
-        //sendFeedbackMessage("halfRep:$halfRep\r\n percentage = $percentage")
-
-
         sendProgressUpdate(percentage.toFloat())
-
-        /*sendFeedbackMessage(
-            "angleLeft:" + String.format("%.2f", angleKneeLeft) + "\r\n" +
-            "angleRight:" + String.format("%.2f", angleKneeRight) + "\r\n" +
-                    "anglHipLeft:" + String.format("%.2f", angleHipLeft) + "\r\n" +
-                    "angleHipRight:" + String.format("%.2f", angleHipRight) + "\r\n" +
-            "completionLeft:" + String.format("%.2f", completionLeft) + "\r\n" +
-            "completionRight:" + String.format("%.2f", completionRight) + "\r\n" +
-            "currentOrientation:" + String.format("%.2f", currentOrientation) + "\r\n"
-        )*/
     }
 
     @SuppressLint("DefaultLocale")
@@ -210,8 +167,6 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
             completion = max(completionHipLeft * 0.3, completionHipRight * 0.3) + max((completionLeft * 0.3), (completionRight * 0.3)) + (completionKneeToAnkle * 0.4)
         else
             completion = min(completionLeft, completionRight)
-
-        //completion = (completionLeft + completionRight) / 2
 
         movingAverage.add(completion)
 
@@ -246,14 +201,7 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
             halfRep = false
         }
 
-        //sendFeedbackMessage("halfRep:$halfRep\r\n percentage = $percentage")
-
         sendProgressUpdate(percentage.toFloat())
-
-        /*sendFeedbackMessage(
-                "completionHipLeft:" + String.format("%.2f", completionHipLeft) + "\r\n" +
-                "completionHipRight:" + String.format("%.2f", completionHipRight) + "\r\n" +
-                "currentOrientation: $currentOrientation")*/
     }
 
     private fun preprocessKeypoints(landmarks: List<NormalizedLandmark>): List<Keypoint>{
@@ -300,51 +248,7 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
         val shoulderOrientation = getVectorOrientation(shoulderVector, referenceVector)
         val hipOrientation = getVectorOrientation(hipVector, referenceVector)
 
-        //println("Shoulder: $shoulderOrientation Hip: $hipOrientation")
-
         return (shoulderOrientation + hipOrientation) / 2
-    }
-
-    private fun rotateKeypoints(keypointFrame: List<Keypoint>): List<Keypoint>{
-        val rotatedKeypoints: MutableList<Keypoint> = mutableListOf()
-
-        val rotationAxis = Vector(0.0,0.0,1.0)
-        val angleDegrees = 90.0
-
-        val rotationAngleInRadians = (angleDegrees*PI)/180
-        val cosTheta = cos(rotationAngleInRadians)
-        val sinTheta = sin(rotationAngleInRadians)
-        val oneMinusCosTheta = 1 - cosTheta
-
-        // Normalize the axis
-        val axisMagnitude = sqrt(rotationAxis.x * rotationAxis.x + rotationAxis.y * rotationAxis.y + rotationAxis.z * rotationAxis.z)
-        val uX = rotationAxis.x / axisMagnitude
-        val uY = rotationAxis.y / axisMagnitude
-        val uZ = rotationAxis.z / axisMagnitude
-
-        // Rotation matrix components
-        val r11 = cosTheta + uX * uX * oneMinusCosTheta
-        val r12 = uX * uY * oneMinusCosTheta - uZ * sinTheta
-        val r13 = uX * uZ * oneMinusCosTheta + uY * sinTheta
-
-        val r21 = uY * uX * oneMinusCosTheta + uZ * sinTheta
-        val r22 = cosTheta + uY * uY * oneMinusCosTheta
-        val r23 = uY * uZ * oneMinusCosTheta - uX * sinTheta
-
-        val r31 = uZ * uX * oneMinusCosTheta - uY * sinTheta
-        val r32 = uZ * uY * oneMinusCosTheta + uX * sinTheta
-        val r33 = cosTheta + uZ * uZ * oneMinusCosTheta
-
-        // Apply rotation
-        for(keyPoint in keypointFrame) {
-            val rotatedX = r11 * keyPoint.x  + r12 * keyPoint.y  + r13 * keyPoint.z
-            val rotatedY = r21 * keyPoint.x  + r22 * keyPoint.y  + r23 * keyPoint.z
-            val rotatedZ = r31 * keyPoint.x  + r32 * keyPoint.y  + r33 * keyPoint.z
-
-            rotatedKeypoints.add(Keypoint(rotatedX, rotatedY, rotatedZ))
-        }
-        //println("rotated!!")
-        return rotatedKeypoints
     }
 
     private fun getVectorOrientation(left: Vector, right: Vector):Double{
@@ -354,139 +258,6 @@ class ExerciseRepCounterImpl : ExerciseRepCounter() {
 
         return acos(dot / (magnitudeLeft*magnitudeRight))*(180/PI)
     }
-    // Calculate a vector between two keypoints
-    fun calculateVector(from: Keypoint, to: Keypoint): Keypoint {
-        return Keypoint(to.x - from.x, to.y - from.y, to.z - from.z)
-    }
-
-    // Normalize a vector
-    fun normalise(vector: Keypoint): Keypoint {
-        val magnitude = sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z)
-        return if (magnitude > 0) Keypoint(vector.x / magnitude, vector.y / magnitude, vector.z / magnitude) else vector
-    }
-
-    private fun checkForGlitch(previousFrame: List<Keypoint>, currentFrame: List<Keypoint>, threshold: Float = 0.3f): List<Keypoint>{
-        var result = currentFrame
-        val velocityThreshold = 0.015f
-
-        for(keypoint in currentFrame.indices){
-            val dx = currentFrame[keypoint].x  - previousFrame[keypoint].x
-            val dy = currentFrame[keypoint].y  - previousFrame[keypoint].y
-            val dz = currentFrame[keypoint].z  - previousFrame[keypoint].z
-
-            val displacement = sqrt(dx * dx + dy * dy + dz * dz)
-
-            val velocity = displacement / (currentTimestamp  - previousTimestamp)
-
-
-            if(displacement > threshold || velocity > velocityThreshold){
-                //println(displacement)
-                //println("Glitch Detected")
-                sendFeedbackMessage(
-                "displacement:" + String.format("%.8f", displacement) + "\r\n" +
-                "velocity:" + String.format("%.8f", velocity) + "\r\n" +
-                "currentOrientation: $currentOrientation")
-                result = previousFrame
-                break
-            }
-        }
-
-
-
-        return result
-    }
-    /*private fun adjustZAxisPredictions(rotatedKeypoints: List<Keypoint>,currentKeypoints: List<Keypoint>): List<Keypoint>{
-        val hipL = 23
-        val hipR = 24
-        val kneeL = 25
-        val kneeR = 26
-        val ankleL = 27
-        val ankleR = 28
-
-        var isKneeLeft = false
-        var isAnkleLeft = false
-
-        var adjustedKeypoints: MutableList<Keypoint> = mutableListOf()
-
-        val kneeHipVectorR = calculateVector(currentKeypoints[hipR], currentKeypoints[kneeR])
-        val kneeHipVectorL = calculateVector(currentKeypoints[hipL], currentKeypoints[kneeL])
-        val kneeHipMagnitudeR = sqrt(kneeHipVectorR.x * kneeHipVectorR.x + kneeHipVectorR.y * kneeHipVectorR.y + kneeHipVectorR.z * kneeHipVectorR.z)
-        val kneeHipMagnitudeL = sqrt(kneeHipVectorL.x * kneeHipVectorL.x + kneeHipVectorL.y * kneeHipVectorL.y + kneeHipVectorL.z * kneeHipVectorL.z)
-
-        var kneeHipMagnitude = 0.0f
-
-        if(kneeHipMagnitudeR < kneeHipMagnitudeR){
-            isKneeLeft = true
-            kneeHipMagnitude = kneeHipMagnitudeL.toFloat()
-        }
-        else{
-            isKneeLeft = false
-            kneeHipMagnitude = kneeHipMagnitudeR.toFloat()
-        }
-
-        val kneeAnkleVectorR = calculateVector(currentKeypoints[ankleR], currentKeypoints[kneeR])
-        val kneeAnkleMagnitudeR = sqrt(kneeAnkleVectorR.x * kneeAnkleVectorR.x + kneeAnkleVectorR.y * kneeAnkleVectorR.y + kneeAnkleVectorR.z * kneeAnkleVectorR.z)
-        val kneeAnkleVectorL = calculateVector(currentKeypoints[ankleL], currentKeypoints[kneeL])
-        val kneeAnkleMagnitudeL = sqrt(kneeAnkleVectorL.x * kneeAnkleVectorL.x + kneeAnkleVectorL.y * kneeAnkleVectorL.y + kneeAnkleVectorL.z * kneeAnkleVectorL.z)
-
-        var kneeAnkleMagnitude = 0.0f
-        if(kneeAnkleMagnitudeR < kneeAnkleMagnitudeL){
-            isAnkleLeft = true
-            kneeAnkleMagnitude = kneeAnkleMagnitudeL.toFloat()
-        }
-        else{
-            isAnkleLeft = false
-            kneeAnkleMagnitude = kneeAnkleMagnitudeR.toFloat()
-        }
-        val vectorKnee: Keypoint
-        val vectorAnkle:Keypoint
-
-        if(isKneeLeft){
-            vectorKnee = calculateVector(currentKeypoints[hipL], rotatedKeypoints[kneeL])
-            vectorAnkle = calculateVector(currentKeypoints[ankleR], rotatedKeypoints[kneeR])
-        }
-        else{
-            vectorKnee = calculateVector(currentKeypoints[ankleR], rotatedKeypoints[kneeR])
-            vectorAnkle = calculateVector(currentKeypoints[ankleL], rotatedKeypoints[kneeL])
-        }
-
-        val normalisedKnee = normalise(vectorKnee)
-        val normalizedAnkle = normalise(vectorAnkle)
-
-        val newKnee: Keypoint
-        val newAnkle: Keypoint
-        adjustedKeypoints = currentKeypoints.toMutableList()
-
-        if(isKneeLeft) {
-            newKnee = Keypoint(
-                currentKeypoints[hipL].x + normalisedKnee.x * kneeHipMagnitude,
-                currentKeypoints[hipL].y + normalisedKnee.y * kneeHipMagnitude,
-                currentKeypoints[hipL].z + normalisedKnee.z * kneeHipMagnitude
-            )
-            newAnkle = Keypoint(
-                rotatedKeypoints[kneeR].x + normalizedAnkle.x * kneeAnkleMagnitude,
-                rotatedKeypoints[kneeR].y + normalizedAnkle.y * kneeAnkleMagnitude,
-                rotatedKeypoints[kneeR].z + normalizedAnkle.z * kneeAnkleMagnitude
-            )
-            adjustedKeypoints[kneeL] = newKnee
-            adjustedKeypoints[ankleL] = newAnkle
-        }else{
-            newKnee = Keypoint(
-                currentKeypoints[hipR].x + normalisedKnee.x * kneeHipMagnitude,
-                currentKeypoints[hipR].y + normalisedKnee.y * kneeHipMagnitude,
-                currentKeypoints[hipR].z + normalisedKnee.z * kneeHipMagnitude
-            )
-            newAnkle = Keypoint(
-                rotatedKeypoints[kneeL].x + normalizedAnkle.x * kneeAnkleMagnitude,
-                rotatedKeypoints[kneeL].y + normalizedAnkle.y * kneeAnkleMagnitude,
-                rotatedKeypoints[kneeL].z + normalizedAnkle.z * kneeAnkleMagnitude
-            )
-
-            adjustedKeypoints[kneeR] = newKnee
-            adjustedKeypoints[ankleR] = newAnkle
-        }
-        return adjustedKeypoints
-    }  */
 }
 class MovingAverage(private val windowSize: Int) {
     private var values = mutableListOf<Double>()
